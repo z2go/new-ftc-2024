38a5cac7c139360268d0824b77bb098df9417d3c
Alisa1098 <118067084+Alisa1098@users.noreply.github.com>
2024-12-07 13:32:41 -0500

    teleO= + encoderAuton

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AdvancedTele.java
@@ -0,0 +1,194 @@
+
+package org.firstinspires.ftc.teamcode.teleop;
+
+
+
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+
+
+@TeleOp(name="AdvancedTele", group="Iterative Opmode")
+public class AdvancedTele extends OpMode {
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
+    //private DcMotor rightSlide1;
+    //private DcMotor rightSlide2;
+    //private DcMotor leftSlide1;
+    //private DcMotor leftSlide2;
+    // private DcMotor outtake;
+    //private Servo launcher;
+    private Servo claw;
+    //private Servo pixelDrop;
+    private boolean halfPower = false;
+
+    private boolean servoIsActivated = false;
+    // TODO: Set any constant values here, if necessary
+    // Example: private final double MAX_POWER = 1.0;
+    private final double MAX_POWER = 1.0;
+    @Override
+    public void init() {
+        telemetry.addData("Status", "Initializing");
+
+
+
+
+        // TODO: Initialize motors and servos here
+        frontLeft = hardwareMap.get(DcMotor.class,"fl");
+        frontRight = hardwareMap.get(DcMotor.class,"fr");
+        backLeft = hardwareMap.get(DcMotor.class,"bl");
+        backRight = hardwareMap.get(DcMotor.class,"br");
+
+        //rightSlide1 = hardwareMap.get(DcMotor.class,"rightSlide1");
+        //rightSlide2 = hardwareMap.get(DcMotor.class,"rightSlide2");
+        //leftSlide1 = hardwareMap.get(DcMotor.class,"leftSlide1");
+        //leftSlide2 = hardwareMap.get(DcMotor.class,"leftSlide2");
+
+
+        // outtake = hardwareMap.get(DcMotor.class,"outtake");
+        //launcher = hardwareMap.get(Servo.class, "launcher");
+        claw = hardwareMap.get(Servo.class, "claw");
+
+        // Hint: Use hardwareMap.get() method
+        // Example: frontLeft = hardwareMap.get(DcMotor.class, "front_left_motor");
+
+
+
+
+        // TODO: Set motor directions and modes here.
+        // Hint: You'll have to reverse some motors to drive straight -- you can figure out which ones through trial and error!
+        // Hint: Use motor.setDirection() and motor.setMode() methods
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        backLeft.setDirection(DcMotor.Direction.FORWARD);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
+        frontLeft.setDirection(DcMotor.Direction.FORWARD);
+
+        //rightSlide1.setDirection(DcMotor.Direction.FORWARD);
+        //rightSlide2.setDirection(DcMotor.Direction.FORWARD);
+        //leftSlide1.setDirection(DcMotor.Direction.FORWARD);
+        //leftSlide2.setDirection(DcMotor.Direction.FORWARD);
+        // outtake.setDirection(DcMotor.Direction.FORWARD);
+
+
+
+
+        frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        // outtake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        //rightSlide1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        //rightSlide2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        //leftSlide1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        //leftSlide2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        // Tell the driver that initialization is complete.
+        telemetry.addData("Status", "Initialized");
+    }
+
+
+
+
+    @Override
+    public void init_loop() {
+        // This is optional: Here we add any code that needs to run repeatedly during initialization
+    }
+
+
+
+
+    @Override
+    public void start() {
+        runtime.reset();
+        // This is optional: Here we add code to be run once when the PLAY button is hit
+    }
+    @Override
+    public void loop() {
+        double leftTrigger = gamepad1.left_trigger;
+        double rightTrigger = gamepad1.right_trigger;
+        double power = gamepad1.left_stick_y;
+        double strafe = gamepad1.left_stick_x;
+        double turn = gamepad1.right_stick_x;
+
+        servoIsActivated = gamepad1.a;
+        claw.setPosition(servoIsActivated?1:0);
+
+            if (gamepad1.b) {
+                //launcher.setPosition(-1);
+            }
+
+            if (gamepad1.a) {
+                //launcher.setPosition(0);
+            }
+                // if (gamepad1.left_bumper) {
+                //      outtake.setPower(1);
+                //  }
+                // if (gamepad1.right_bumper) {
+                //     outtake.setPower(-1);
+                // }
+
+                if (halfPower) {
+                    power *= 0.5;
+                    strafe *= 0.5;
+                    turn *= 0.5;
+                }
+                double flPower = power-strafe-turn;
+                double blPower = power+strafe-turn;
+                double frPower = power+strafe+turn;
+                double brPower = power-strafe+turn;
+
+
+                frontLeft.setPower(flPower);
+                backLeft.setPower(blPower);
+                backRight.setPower(brPower);
+                frontRight.setPower(frPower);
+                if (leftTrigger > 0) {
+                    //rightSlide1.setPower(leftTrigger);
+                    //rightSlide2.setPower(-leftTrigger);
+                    //leftSlide1.setPower(-leftTrigger);
+                    //leftSlide2.setPower(leftTrigger);
+                }
+                if (rightTrigger > 0) {
+                    //leftSlide1.setPower(rightTrigger);
+                    //leftSlide2.setPower(-rightTrigger);
+                    // rightSlide1.setPower(-rightTrigger);
+                    //rightSlide2.setPower(rightTrigger);
+                }
+
+
+            }
+
+
+
+
+            @Override
+            public void stop() {
+                // TODO: Stop all motors and any additional components
+                backLeft.setPower(0);
+                frontLeft.setPower(0);
+                frontRight.setPower(0);
+                backRight.setPower(0);
+
+                //leftSlide1.setPower(0);
+                //leftSlide2.setPower(0);
+                //rightSlide1.setPower(0);
+                //rightSlide2.setPower(0);
+                // outtake.setPower(0);
+                // Hint: Set all motor powers to 0
+            }
+
+
+
+
+        }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonEncoders.java
@@ -0,0 +1,165 @@
+package org.firstinspires.ftc.teamcode.Auton;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+@Autonomous(name = "auton w encoders", group = "Auto")
+public class AutonEncoders extends LinearOpMode {
+
+    //TeamElementSubsystem teamElementDetection = null;
+    boolean togglePreview = true;
+
+    // Drive constants for encoder setup:
+    static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
+    static final double     DRIVE_GEAR_REDUCTION    = 2.0 ;     // This is < 1.0 if geared UP
+    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double     DRIVE_SPEED             = 0.6;
+    static final double     TURN_SPEED              = 0.5;
+
+    // Motors:
+    DcMotor leftFront;
+    DcMotor rightFront;
+    DcMotor leftBack;
+    DcMotor rightBack;
+
+
+    //The distance we want to travel, the speed we want to move at, and the distance translated into
+    //an encoder target
+    double dist;
+    double power;
+    int target;
+
+    String direc;
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+
+    public void HardwareStart() {
+
+        telemetry.addData("Object Creation", "Start");
+        telemetry.update();
+
+        //teamElementDetection = new TeamElementSubsystem(hardwareMap);
+
+        telemetry.addData("Object Creation", "Done");
+        telemetry.update();
+
+    }
+    public void runOpMode() {
+
+        //this might need to be redone between different times when you use the encoder drive method:
+        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+        //put the specific encoder drive stuff w the measurments that we want here:
+
+    }
+
+    public void encoderDrive(double timeout) {
+        double ROTATIONS = dist / (Math.PI * WHEEL_DIAMETER_INCHES);
+        /*if(direc.equals("forward") || direc.equals("backward")) {
+            target = leftFront.getCurrentPosition() + (int) (dist * COUNTS_PER_INCH);
+        }*/
+        double counts =  COUNTS_PER_MOTOR_REV * ROTATIONS * DRIVE_GEAR_REDUCTION;
+        target = leftFront.getCurrentPosition() + (int) counts;
+        switch (direc) {
+            case "Forward": // robot will move forward
+                leftFront.setTargetPosition((int) target);
+                rightFront.setTargetPosition((int) target);
+                leftBack.setTargetPosition((int) target);
+                rightBack.setTargetPosition((int) target);
+                break;
+            case "Backward": // robot will move backward
+                leftFront.setTargetPosition((int) -target);
+                rightFront.setTargetPosition((int) -target);
+                leftBack.setTargetPosition((int) -target);
+                rightBack.setTargetPosition((int) -target);
+                break;
+            case "Left": // robot will strafe left
+                leftFront.setTargetPosition((int) -target);
+                rightFront.setTargetPosition((int) target);
+                leftBack.setTargetPosition((int) target);
+                rightBack.setTargetPosition((int) -target);
+                break;
+            case "Right": // robot will strafe right
+                leftFront.setTargetPosition((int) target);
+                rightFront.setTargetPosition((int) -target);
+                leftBack.setTargetPosition((int) -target);
+                rightBack.setTargetPosition((int) target);
+                break;
+            case "RLeft": // robot will rotate left
+                leftFront.setTargetPosition((int) -counts);
+                rightFront.setTargetPosition((int) counts);
+                leftBack.setTargetPosition((int) -counts);
+                rightBack.setTargetPosition((int) counts);
+                break;
+            case "RRight": // robot will rotate right
+                leftFront.setTargetPosition((int) counts);
+                rightFront.setTargetPosition((int) -counts);
+                leftBack.setTargetPosition((int) counts);
+                rightBack.setTargetPosition((int) -counts);
+                break;
+        }
+        /*leftFront.setTargetPosition(target);
+        leftBack.setTargetPosition(target);
+        rightFront.setTargetPosition(target);
+        rightBack.setTargetPosition(target);*/
+
+        runtime.reset();
+
+        if(direc.equals("Forward") || direc.equals("Backward")) {
+            leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            leftFront.setPower(power);
+            leftBack.setPower(power);
+            rightFront.setPower(power);
+            rightBack.setPower(power);
+
+            while (leftFront.isBusy()
+                    && rightFront.isBusy()
+                    && leftBack.isBusy()
+                    && rightBack.isBusy()
+                    && !(runtime.seconds() > timeout) ) {
+            }
+        }
+
+        stop(leftFront, rightFront, leftBack, rightBack);
+
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    }
+
+    public void stop(DcMotor motorlf, DcMotor motorrf, DcMotor motorlb, DcMotor motorrb) {
+        //robot stops moving
+        motorlf.setPower(0.0);
+        motorrf.setPower(0.0);
+        motorlb.setPower(0.0);
+        motorrb.setPower(0.0);
+    }
+
+
+}

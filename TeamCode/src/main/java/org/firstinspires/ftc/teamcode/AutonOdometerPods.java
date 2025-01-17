e4ecc68714f5959b46dcc05bf3171c54a4119b92
Alisa1098 <118067084+Alisa1098@users.noreply.github.com>
2025-01-16 23:40:13 -0500

    updating auton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonOdometerPods.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonOdometerPods.java
@@ -26,22 +26,33 @@ public class AutonOdometerPods extends LinearOpMode{
             // Note, this example takes more than 30 seconds to execute, so turn OFF the auto timer.
 
             // Drive a large rectangle, turning at each corner
-            robot.drive(  84, 0.60, 0.25);
+            /*robot.drive(  24.5, 0.60, 0.25);
             robot.turnTo(90, 0.45, 0.5);
             robot.drive(  72, 0.60, 0.25);
             robot.turnTo(180, 0.45, 0.5);
             robot.drive(  84, 0.60, 0.25);
             robot.turnTo(270, 0.45, 0.5);
             robot.drive(  72, 0.60, 0.25);
-            robot.turnTo(0, 0.45, 0.5);
+            robot.turnTo(0, 0.45, 0.5);*/
 
-            sleep(500);
+            //sleep(500);
 
-            // Drive the path again without turning.
-            robot.drive(  84, 0.60, 0.15);
-            robot.strafe( 72, 0.60, 0.15);
-            robot.drive( -84, 0.60, 0.15);
-            robot.strafe(-72, 0.60, 0.15);
+
+            // PUT ALL OF THIS IN A LOOP (must repeat as many times as will fit in 30 seconds)
+
+            //DRIVING TO THE SUBMERISBLE ZONE
+            robot.drive(  24.5, 0.60, 0.15);
+            robot.strafe( 24.5, 0.60, 0.15);
+
+            //DO SPECIMEN STUFF HERE
+
+            //DRIVING BACK TO GET NEW SAMPLE
+            robot.strafe(-24.5, 0.60, 0.15);
+            robot.drive( -24.5, 0.60, 0.15);
+
+            //GET NEW SPECIMEN FROM WALL HERE
+
         }
+
     }
 }
